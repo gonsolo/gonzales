aff4615e6c154e8a17576a059eddf698eb8d46af
Gon Solo <gonsolo@gmail.com>
2022-12-13 13:18:26 +0100

    Format Package.swift.

--- Package.swift
+++ Package.swift
@@ -6,38 +6,59 @@ let package = Package(
         name: "gonzales",
         platforms: [.macOS(.v10_15)],
         dependencies: [
-            .package(name: "swift-png", url: "https://github.com/kelvin13/png.git", from: "4.0.2"),
+                .package(
+                        name: "swift-png",
+                        url: "https://github.com/kelvin13/png.git",
+                        from: "4.0.2")
         ],
-        //.product(name: "png", package: "PNG")
         targets: [
-                .target(name: "gonzales",
-                        dependencies: [.product(name: "PNG", package: "swift-png"), "exr", "embree", "ptex"],
-                        linkerSettings: [.unsafeFlags([
-                                "-LExtern/ptex/build/src/ptex",
-                                "-lPtex"
-                        ])]
+                .target(
+                        name: "gonzales",
+                        dependencies: [
+                                .product(
+                                        name: "PNG",
+                                        package: "swift-png"),
+                                "exr",
+                                "embree",
+                                "ptex",
+                        ],
+                        linkerSettings: [
+                                .unsafeFlags([
+                                        "-LExtern/ptex/build/src/ptex",
+                                        "-lPtex",
+                                ])
+                        ]
                 ),
-                .target(name: "exr",
-                        cxxSettings: [.unsafeFlags([
-                                "-IExtern/openexr/src/lib/OpenEXR",
-                                "-IExtern/openexr/build/cmake",
-                                "-IExtern/openexr/build/_deps/imath-src/src/Imath",
-                                "-IExtern/openexr/build/_deps/imath-build/config",
-                                "-IExtern/openexr/src/lib/Iex"
-                        ])],
-                        linkerSettings: [.unsafeFlags([
-                                "-LExtern/openexr/build/src/lib/OpenEXR",
-                                "-lOpenEXR-3_2",
-                                "-LExtern/openexr/build/_deps/imath-build/src/Imath",
-                                "-lImath-3_2"
-                        ])]
+                .target(
+                        name: "exr",
+                        cxxSettings: [
+                                .unsafeFlags([
+                                        "-IExtern/openexr/src/lib/OpenEXR",
+                                        "-IExtern/openexr/build/cmake",
+                                        "-IExtern/openexr/build/_deps/imath-src/src/Imath",
+                                        "-IExtern/openexr/build/_deps/imath-build/config",
+                                        "-IExtern/openexr/src/lib/Iex",
+                                ])
+                        ],
+                        linkerSettings: [
+                                .unsafeFlags([
+                                        "-LExtern/openexr/build/src/lib/OpenEXR",
+                                        "-lOpenEXR-3_2",
+                                        "-LExtern/openexr/build/_deps/imath-build/src/Imath",
+                                        "-lImath-3_2",
+                                ])
+                        ]
                 ),
-                .target(name: "ptex",
+                .target(
+                        name: "ptex",
                         dependencies: [],
-                        cxxSettings: [.unsafeFlags([
-                                "-IExtern/ptex/src/ptex"
-                        ])]),
-                .target(name: "embree",
+                        cxxSettings: [
+                                .unsafeFlags([
+                                        "-IExtern/ptex/src/ptex"
+                                ])
+                        ]),
+                .target(
+                        name: "embree",
                         dependencies: [],
                         cxxSettings: [],
                         linkerSettings: [

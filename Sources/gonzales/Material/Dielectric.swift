a65d83d2d12ba954b7f4643057b3aa42587f1470
Gon Solo <gonsolo@gmail.com>
2023-01-20 18:14:48 +0100

    Add quick'n'dirty Dielectric (via Glass).

--- Sources/gonzales/Material/Dielectric.swift
+++ Sources/gonzales/Material/Dielectric.swift
@@ -1,15 +1,33 @@
 final class Dielectric: Material {
 
-        init() {}
+        init(reflectance: RGBSpectrumTexture, transmittance: RGBSpectrumTexture, eta: FloatTexture)
+        {
+                self.reflectance = reflectance
+                self.transmittance = transmittance
+                self.eta = eta
+        }
 
         func computeScatteringFunctions(interaction: Interaction) -> BSDF {
-                // TODO
                 var bsdf = BSDF(interaction: interaction)
-                bsdf.set(bxdf: LambertianReflection(reflectance: white))
+                let eta = self.eta.evaluateFloat(at: interaction)
+                bsdf.eta = eta
+                let reflectance = self.reflectance.evaluateRGBSpectrum(at: interaction)
+                let transmittance = self.transmittance.evaluateRGBSpectrum(at: interaction)
+                if reflectance.isBlack && transmittance.isBlack { return bsdf }
+                let specular = FresnelSpecular(
+                        reflectance: reflectance, transmittance: transmittance, etaA: 1, etaB: eta)
+                bsdf.set(bxdf: specular)
                 return bsdf
         }
+
+        var reflectance: RGBSpectrumTexture
+        var transmittance: RGBSpectrumTexture
+        var eta: FloatTexture
 }
 
 func createDielectric(parameters: ParameterDictionary) throws -> Dielectric {
-        return Dielectric()
+        let kr = try parameters.findRGBSpectrumTexture(name: "Kr", else: RGBSpectrum(intensity: 1))
+        let kt = try parameters.findRGBSpectrumTexture(name: "Kt", else: RGBSpectrum(intensity: 1))
+        let eta = try parameters.findFloatXTexture(name: "eta", else: 1.5)
+        return Dielectric(reflectance: kr, transmittance: kt, eta: eta)
 }

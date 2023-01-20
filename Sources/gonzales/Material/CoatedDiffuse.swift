db76475c5209c54ff6da3c6d8862ba227e083c8f
Gon Solo <gonsolo@gmail.com>
2023-01-20 17:55:10 +0100

    Quick'n'dirty coated diffuse with lambertian.

--- Sources/gonzales/Material/CoatedDiffuse.swift
+++ Sources/gonzales/Material/CoatedDiffuse.swift
@@ -1,15 +1,29 @@
 final class CoatedDiffuse: Material {
 
-        init() {}
+        init(roughness: (FloatX, FloatX), reflectance: RGBSpectrumTexture) {
+                self.roughness = roughness
+                self.reflectance = reflectance
+        }
 
         func computeScatteringFunctions(interaction: Interaction) -> BSDF {
-                // TODO
                 var bsdf = BSDF(interaction: interaction)
-                bsdf.set(bxdf: LambertianReflection(reflectance: white))
+                let reflectanceAtInteraction = reflectance.evaluateRGBSpectrum(at: interaction)
+                bsdf.set(bxdf: LambertianReflection(reflectance: reflectanceAtInteraction))
                 return bsdf
         }
+
+        var roughness: (FloatX, FloatX)
+        var reflectance: RGBSpectrumTexture
 }
 
 func createCoatedDiffuse(parameters: ParameterDictionary) throws -> CoatedDiffuse {
-        return CoatedDiffuse()
+        // remaproughness assumed to be false
+        let roughnessOptional = try parameters.findOneFloatXOptional(called: "roughness")
+        let uRoughness =
+                try roughnessOptional ?? parameters.findOneFloatX(called: "uroughness", else: 0.5)
+        let vRoughness =
+                try roughnessOptional ?? parameters.findOneFloatX(called: "vroughness", else: 0.5)
+        let roughness = (uRoughness, vRoughness)
+        let reflectance = try parameters.findRGBSpectrumTexture(name: "reflectance")
+        return CoatedDiffuse(roughness: roughness, reflectance: reflectance)
 }

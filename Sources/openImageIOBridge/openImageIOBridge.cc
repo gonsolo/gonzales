de844d3e9aea4172a86e58733762aa722e9b7dd7
Gon Solo <gonsolo@gmail.com>
2023-03-21 14:36:40 +0100

    Format again (sigh).

--- Sources/openImageIOBridge/openImageIOBridge.cc
+++ Sources/openImageIOBridge/openImageIOBridge.cc
@@ -19,8 +19,7 @@ bool texture(const char *filename_c, float s, float t, float result[3]) {
         float dsdy = 0;
         float dtdy = 0;
         int nchannels = 3;
-        return textureSystem->texture(filename, options, s, t, dsdx, dtdx, dsdy, dtdy, nchannels,
-                                      result);
+        return textureSystem->texture(filename, options, s, t, dsdx, dtdx, dsdy, dtdy, nchannels, result);
 }
 
 void writeImage(const char *filename_c, const float *pixels, const int xres, const int yres) {

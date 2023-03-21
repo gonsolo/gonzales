b53d47d9973e4781b80a9e2eed21392737011f85
Gon Solo <gonsolo@gmail.com>
2023-03-21 14:33:47 +0100

    Clang format.

--- Sources/openImageIOBridge/include/openImageIOBridge.h
+++ Sources/openImageIOBridge/include/openImageIOBridge.h
@@ -4,10 +4,9 @@ extern "C" {
 
 void createTextureSystem();
 void destroyTextureSystem();
-bool texture(const char* filename_c, float s, float t, float result[3]);
-void writeImage(const char* filename_c, const float* pixels, const int xres, const int yres);
+bool texture(const char *filename_c, float s, float t, float result[3]);
+void writeImage(const char *filename_c, const float *pixels, const int xres, const int yres);
 
 #ifdef __cplusplus
 }
 #endif
-

01c2888cc9ed70fc5d9bba2097dfc519e924d6ba
Gon Solo <gonsolo@gmail.com>
2023-03-24 17:26:34 +0100

    Add skeleton for coated.

--- Sources/gonzales/Bsdf/CoatedDiffuseBsdf.swift
+++ Sources/gonzales/Bsdf/CoatedDiffuseBsdf.swift
@@ -1,15 +1,30 @@
 struct CoatedDiffuseBxdf: BxDF {
 
+        init(reflectance: RGBSpectrum, roughness: (FloatX, FloatX)) {
+                self.reflectance = reflectance
+                self.roughness = roughness
+                self.topBxdf = Dielectric()
+                self.bottomBxdf = DiffuseBxdf(reflectance: reflectance)
+        }
+
         func evaluate(wo: Vector, wi: Vector) -> RGBSpectrum {
                 // TODO
-                return reflectance / FloatX.pi
+                return bottomBxdf.evaluate(wo: wo, wi: wi)
         }
+
+        //func sample(wo: Vector, u: Point2F) -> (RGBSpectrum, Vector, FloatX) {
+        //        unimplemented()
+        //}
+
+        //func probabilityDensity(wo: Vector, wi: Vector) -> FloatX {
+        //        unimplemented()
+        //}
 
         func albedo() -> RGBSpectrum { return reflectance }
 
         let reflectance: RGBSpectrum
         let roughness: (FloatX, FloatX)
 
-        let topBxdf = Dielectric()
-        let bottomBxdf = DiffuseBxdf()
+        let topBxdf: Dielectric
+        let bottomBxdf: DiffuseBxdf
 }

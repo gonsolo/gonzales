62a405ec08cfe6bec735f598b8a30d055164d1b6
Gon Solo <gonsolo@gmail.com>
2023-07-12 11:58:59 +0200

    Print device name.

--- Sources/gonzales/Accelerators/Optix.swift
+++ Sources/gonzales/Accelerators/Optix.swift
@@ -1,7 +1,31 @@
 import cuda
 
+func cudaCheck(_ cudaError: cudaError_t) {
+        if cudaError != cudaSuccess {
+                print("Cuda error: \(cudaError)")
+        }
+}
+
 func printCudaDevices() {
         var numDevices: Int32 = 0
-        cudaGetDeviceCount(&numDevices)
+        var cudaError: cudaError_t
+        cudaError = cudaGetDeviceCount(&numDevices)
+        cudaCheck(cudaError)
         print("Cuda device count: \(numDevices)")
+
+        var cudaDevice: Int32 = 0
+        cudaError = cudaGetDevice(&cudaDevice)
+        cudaCheck(cudaError)
+        print("Cuda device used: \(cudaDevice)")
+
+        var cudaDeviceProperties: cudaDeviceProp = cudaDeviceProp()
+        cudaError = cudaGetDeviceProperties_v2(&cudaDeviceProperties, cudaDevice)
+        cudaCheck(cudaError)
+
+        let name = withUnsafePointer(to: cudaDeviceProperties.name) {
+                $0.withMemoryRebound(to: UInt8.self, capacity: MemoryLayout.size(ofValue: $0)) {
+                        String(cString: $0)
+                }
+        }
+        print(name)
 }

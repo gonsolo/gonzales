60dbe4fd02e6312be1059a9cc86396c7a2885c7f
Gon Solo <gonsolo@gmail.com>
2023-10-19 10:57:33 +0200

    Affine space not needed.

--- Sources/cudaBridge/Model.h
+++ Sources/cudaBridge/Model.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "gdt/math/AffineSpace.h"
+#include "gdt/math/vec.h"
+
 #include <vector>
 
 struct TriangleMesh {
@@ -15,14 +16,9 @@ struct TriangleMesh {
       void addTriangle(float ax, float ay, float az, float bx, float by, float bz, float cx, float cy, float cz)
       {
           int firstVertexID = (int)vertex.size();
-	  gdt::affine3f xfm;
-          xfm.p = gdt::vec3f(0.f, 0.f, 0.f);
-          xfm.l.vx = gdt::vec3f(1.f ,0.f, 0.f);
-          xfm.l.vy = gdt::vec3f(0.f, 1.f, 0.f);
-          xfm.l.vz = gdt::vec3f(0.f, 0.f, 1.f);
-          vertex.push_back(xfmPoint(xfm, gdt::vec3f( ax, ay, az)));
-          vertex.push_back(xfmPoint(xfm, gdt::vec3f( bx, by, bz)));
-          vertex.push_back(xfmPoint(xfm, gdt::vec3f( cx, cy, cz)));
+          vertex.push_back(gdt::vec3f( ax, ay, az));
+          vertex.push_back(gdt::vec3f( bx, by, bz));
+          vertex.push_back(gdt::vec3f( cx, cy, cz));
       
           int indices[] = {0, 1, 2};
           index.push_back(firstVertexID+gdt::vec3i(indices[0], indices[1], indices[2]));

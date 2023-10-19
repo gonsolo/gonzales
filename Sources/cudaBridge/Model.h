5575793781e6f50d04385bba2e2caab8f0e92632
Gon Solo <gonsolo@gmail.com>
2023-10-19 10:55:15 +0200

    Use gdt::.

--- Sources/cudaBridge/Model.h
+++ Sources/cudaBridge/Model.h
@@ -3,35 +3,30 @@
 #include "gdt/math/AffineSpace.h"
 #include <vector>
 
-using namespace gdt;
-
 struct TriangleMesh {
-  std::vector<vec3f> vertex;
-  std::vector<vec3f> normal;
-  std::vector<vec2f> texcoord;
-  std::vector<vec3i> index;
+  std::vector<gdt::vec3f> vertex;
+  std::vector<gdt::vec3f> normal;
+  std::vector<gdt::vec2f> texcoord;
+  std::vector<gdt::vec3i> index;
 
-  vec3f              diffuse;
+  gdt::vec3f              diffuse;
   int                diffuseTextureID { -1 };
 
       void addTriangle(float ax, float ay, float az, float bx, float by, float bz, float cx, float cy, float cz)
       {
           int firstVertexID = (int)vertex.size();
-          affine3f xfm;
-          xfm.p = vec3f(0.f, 0.f, 0.f);
-          xfm.l.vx = vec3f(1.f ,0.f, 0.f);
-          xfm.l.vy = vec3f(0.f, 1.f, 0.f);
-          xfm.l.vz = vec3f(0.f, 0.f, 1.f);
-          vertex.push_back(xfmPoint(xfm, vec3f( ax, ay, az)));
-          vertex.push_back(xfmPoint(xfm, vec3f( bx, by, bz)));
-          vertex.push_back(xfmPoint(xfm, vec3f( cx, cy, cz)));
+	  gdt::affine3f xfm;
+          xfm.p = gdt::vec3f(0.f, 0.f, 0.f);
+          xfm.l.vx = gdt::vec3f(1.f ,0.f, 0.f);
+          xfm.l.vy = gdt::vec3f(0.f, 1.f, 0.f);
+          xfm.l.vz = gdt::vec3f(0.f, 0.f, 1.f);
+          vertex.push_back(xfmPoint(xfm, gdt::vec3f( ax, ay, az)));
+          vertex.push_back(xfmPoint(xfm, gdt::vec3f( bx, by, bz)));
+          vertex.push_back(xfmPoint(xfm, gdt::vec3f( cx, cy, cz)));
       
           int indices[] = {0, 1, 2};
-          index.push_back(firstVertexID+vec3i(indices[0],
-                                              indices[1],
-                                              indices[2]));
+          index.push_back(firstVertexID+gdt::vec3i(indices[0], indices[1], indices[2]));
       }
-
 };
 
 struct Model {

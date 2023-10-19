e41c01bb4ee7c10f28a962ef601149c6d0fc18a8
Gon Solo <gonsolo@gmail.com>
2023-10-19 10:45:27 +0200

    Clean up.

--- Sources/cudaBridge/SampleRenderer.cpp
+++ Sources/cudaBridge/SampleRenderer.cpp
@@ -25,34 +25,15 @@ SampleRenderer::SampleRenderer(const Model *model)
   : model(model)
 {
   initOptix();
-    
-  std::cout << "#osc: creating optix context ..." << std::endl;
   createContext();
-    
-  std::cout << "#osc: setting up module ..." << std::endl;
   createModule();
-
-  std::cout << "#osc: creating raygen programs ..." << std::endl;
   createRaygenPrograms();
-  std::cout << "#osc: creating miss programs ..." << std::endl;
   createMissPrograms();
-  std::cout << "#osc: creating hitgroup programs ..." << std::endl;
   createHitgroupPrograms();
-
   launchParams.traversable = buildAccel();
-  
-  std::cout << "#osc: setting up optix pipeline ..." << std::endl;
   createPipeline();
-
-  std::cout << "#osc: building SBT ..." << std::endl;
   buildSBT();
-
   launchParamsBuffer.alloc(sizeof(launchParams));
-  std::cout << "#osc: context, module, pipeline, etc, all set up ..." << std::endl;
-
-  std::cout << GDT_TERMINAL_GREEN;
-  std::cout << "#osc: Optix 7 Sample fully set up" << std::endl;
-  std::cout << GDT_TERMINAL_DEFAULT;
 }
 
 OptixTraversableHandle SampleRenderer::buildAccel()
@@ -198,24 +179,19 @@ OptixTraversableHandle SampleRenderer::buildAccel()
 /*! helper function that initializes optix and checks for errors */
 void SampleRenderer::initOptix()
 {
-  std::cout << "#osc: initializing optix..." << std::endl;
-    
-  // -------------------------------------------------------
-  // check for available optix7 capable devices
-  // -------------------------------------------------------
   cudaFree(0);
   int numDevices;
   cudaGetDeviceCount(&numDevices);
   if (numDevices == 0)
-    throw std::runtime_error("#osc: no CUDA capable devices found!");
-  std::cout << "#osc: found " << numDevices << " CUDA devices" << std::endl;
+    throw std::runtime_error("no CUDA capable devices found!");
+  std::cout << "Found " << numDevices << " CUDA devices" << std::endl;
 
   // -------------------------------------------------------
   // initialize optix
   // -------------------------------------------------------
   OPTIX_CHECK( optixInit() );
   std::cout << GDT_TERMINAL_GREEN
-            << "#osc: successfully initialized optix... yay!"
+            << "Successfully initialized optix!"
             << GDT_TERMINAL_DEFAULT << std::endl;
 }
 
@@ -237,7 +213,7 @@ void SampleRenderer::createContext()
   CUDA_CHECK(StreamCreate(&stream));
     
   cudaGetDeviceProperties(&deviceProps, deviceID);
-  std::cout << "#osc: running on device: " << deviceProps.name << std::endl;
+  std::cout << "Running on device: " << deviceProps.name << std::endl;
     
   CUresult  cuRes = cuCtxGetCurrent(&cudaContext);
   if( cuRes != CUDA_SUCCESS ) 

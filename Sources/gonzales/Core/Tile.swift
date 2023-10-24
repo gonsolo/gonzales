097c35af709cf74aeeb4a358ccba4fa010b2245b
Gon Solo <gonsolo@gmail.com>
2023-10-24 16:29:47 +0200

    Hoist renderPixel into render.

--- Sources/gonzales/Core/Tile.swift
+++ Sources/gonzales/Core/Tile.swift
@@ -15,45 +15,25 @@ final class Tile {
 
                 var samples = [Sample]()
                 for pixel in bounds {
-                        let pixelSamples = try renderPixel(
-                                pixel: pixel,
-                                reporter: reporter,
-                                sampler: sampler,
-                                camera: camera,
-                                scene: scene,
-                                lightSampler: lightSampler)
-                        samples.append(contentsOf: pixelSamples)
-                }
-                return samples
-        }
-
-        func renderPixel(
-                pixel: Point2I,
-                reporter: ProgressReporter,
-                sampler: Sampler,
-                camera: Camera,
-                scene: Scene,
-                lightSampler: LightSampler
-        ) throws -> [Sample] {
-                var samples = [Sample]()
-                for _ in 0..<sampler.samplesPerPixel {
-                        let cameraSample = sampler.getCameraSample(pixel: pixel)
-                        let ray = camera.generateRay(cameraSample: cameraSample)
-                        var tHit = Float.infinity
-                        let (radiance, albedo, normal) = try integrator.getRadianceAndAlbedo(
-                                from: ray,
-                                tHit: &tHit,
-                                with: sampler,
-                                lightSampler: lightSampler)
-                        let rayWeight: FloatX = 1.0
-                        let sample = Sample(
-                                light: radiance,
-                                albedo: albedo,
-                                normal: normal,
-                                weight: rayWeight,
-                                location: Point2F(x: cameraSample.film.0, y: cameraSample.film.1))
-                        samples.append(sample)
-                        reporter.update()
+                        for _ in 0..<sampler.samplesPerPixel {
+                                let cameraSample = sampler.getCameraSample(pixel: pixel)
+                                let ray = camera.generateRay(cameraSample: cameraSample)
+                                var tHit = Float.infinity
+                                let (radiance, albedo, normal) = try integrator.getRadianceAndAlbedo(
+                                        from: ray,
+                                        tHit: &tHit,
+                                        with: sampler,
+                                        lightSampler: lightSampler)
+                                let rayWeight: FloatX = 1.0
+                                let sample = Sample(
+                                        light: radiance,
+                                        albedo: albedo,
+                                        normal: normal,
+                                        weight: rayWeight,
+                                        location: Point2F(x: cameraSample.film.0, y: cameraSample.film.1))
+                                samples.append(sample)
+                                reporter.update()
+                        }
                 }
                 return samples
         }

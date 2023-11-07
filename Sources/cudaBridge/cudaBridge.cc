19b6959b67e229752dd55c029efe24ef28cbeb30
Gon Solo <gonsolo@gmail.com>
2023-11-07 11:29:06 +0100

    Format.

--- Sources/cudaBridge/cudaBridge.cc
+++ Sources/cudaBridge/cudaBridge.cc
@@ -36,60 +36,52 @@ VectorInt32 primID;
 VectorFloat tMax;
 
 void optixResize(vec2i newSize) {
-		optixRenderer->resize(newSize);
-		auto sizeVec = newSize.x * newSize.y;
-		pixels.resize(sizeVec);
-		vertices.resize(sizeVec);
-		normals.resize(sizeVec);
-		intersected.resize(sizeVec);
-		primID.resize(sizeVec);
-		tMax.resize(sizeVec);
+        optixRenderer->resize(newSize);
+        auto sizeVec = newSize.x * newSize.y;
+        pixels.resize(sizeVec);
+        vertices.resize(sizeVec);
+        normals.resize(sizeVec);
+        intersected.resize(sizeVec);
+        primID.resize(sizeVec);
+        tMax.resize(sizeVec);
 }
 
 void optixSetup() {
         try {
-		model.meshes.push_back(&triangleMesh);
-		optixRenderer = new OptixRenderer(&model);
-		optixResize(vec2i{1, 1});
+                model.meshes.push_back(&triangleMesh);
+                optixRenderer = new OptixRenderer(&model);
+                optixResize(vec2i{1, 1});
         } catch (std::runtime_error &e) {
-		std::cout << "FATAL ERROR: " << e.what() << std::endl;
-		exit(1);
+                std::cout << "FATAL ERROR: " << e.what() << std::endl;
+                exit(1);
         }
 }
 
-void optixIntersectVec(
-	const VectorVec3f& from,
-	const VectorVec3f& dir,
-	VectorFloat &tHit,
-	VectorVec3f &p,
-	VectorVec3f &n,
-	VectorInt32 &didIntersect,
-	VectorInt32 &didPrimID,
-        VectorFloat &didTMax,
-	VectorBool skip
-) {
+void optixIntersectVec(const VectorVec3f &from, const VectorVec3f &dir, VectorFloat &tHit, VectorVec3f &p,
+                       VectorVec3f &n, VectorInt32 &didIntersect, VectorInt32 &didPrimID,
+                       VectorFloat &didTMax, VectorBool skip) {
 #if 1
         try {
-		int size = from.size();
+                int size = from.size();
                 OptixCamera camera = {from, dir, tHit};
-		optixResize(vec2i{size, 1});
+                optixResize(vec2i{size, 1});
                 optixRenderer->setCamera(camera);
                 optixRenderer->render();
                 optixRenderer->downloadPixels(pixels.data(), vertices.data(), normals.data(),
                                               intersected.data(), primID.data(), tMax.data());
-		//std::cout << "intersected size: " << intersected.size() << std::endl;
-		//for (int i = 0; i < 16; i++) {
-		//	std::cout << "intersected: " << intersected[i] << std::endl;
-		//}
-		for (int i = 0; i < size; i++) {
-			if (!skip[i]) {
-				p[i] =  vertices[i];
-				n[i] = normals[i];
-				didIntersect[i] = intersected[i];
-				didPrimID[i] = primID[i];
-				didTMax[i] = tMax[i];
-			}
-		}
+                // std::cout << "intersected size: " << intersected.size() << std::endl;
+                // for (int i = 0; i < 16; i++) {
+                //	std::cout << "intersected: " << intersected[i] << std::endl;
+                // }
+                for (int i = 0; i < size; i++) {
+                        if (!skip[i]) {
+                                p[i] = vertices[i];
+                                n[i] = normals[i];
+                                didIntersect[i] = intersected[i];
+                                didPrimID[i] = primID[i];
+                                didTMax[i] = tMax[i];
+                        }
+                }
         } catch (std::runtime_error &e) {
                 std::cout << "FATAL ERROR: " << e.what() << std::endl;
                 exit(1);
@@ -114,26 +106,17 @@ void optixIntersectVec(
 #endif
 }
 
-void optixIntersect(
-	vec3f from,
-	vec3f dir,
-	float &tHit,
-	vec3f &p,
-	vec3f &n,
-	int &didIntersect,
-	int &didPrimID,
-        float &didTMax,
-	bool skip
-) {
-	if (skip) {
-		return;
-	}
+void optixIntersect(vec3f from, vec3f dir, float &tHit, vec3f &p, vec3f &n, int &didIntersect, int &didPrimID,
+                    float &didTMax, bool skip) {
+        if (skip) {
+                return;
+        }
         try {
-		VectorVec3f fromVec = { from };
-		VectorVec3f dirVec = { dir };
-		VectorFloat tHitVec = { tHit };
+                VectorVec3f fromVec = {from};
+                VectorVec3f dirVec = {dir};
+                VectorFloat tHitVec = {tHit};
                 OptixCamera camera = {fromVec, dirVec, tHitVec};
-		optixResize(vec2i{1, 1});
+                optixResize(vec2i{1, 1});
                 optixRenderer->setCamera(camera);
                 optixRenderer->render();
                 optixRenderer->downloadPixels(pixels.data(), vertices.data(), normals.data(),

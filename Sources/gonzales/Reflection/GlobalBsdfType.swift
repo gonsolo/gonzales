cf0f43f058646b1b6d751c63df903cc707d77095
Gon Solo <gonsolo@gmail.com>
2025-10-14 10:43:08 +0200

    Remove indirect.

--- Sources/gonzales/Reflection/GlobalBsdfType.swift
+++ Sources/gonzales/Reflection/GlobalBsdfType.swift
@@ -1,9 +1,8 @@
-indirect enum GlobalBsdfType: GlobalBsdf {
+enum GlobalBsdfType: GlobalBsdf {
         case coatedDiffuseBsdf(CoatedDiffuseBsdf)
         case dielectricBsdf(DielectricBsdf)
         case diffuseBsdf(DiffuseBsdf)
         case dummyBsdf(DummyBsdf)
-        case geometricPrimitiveBsdf(GlobalBsdfType)  // Special case for recursion
         case hairBsdf(HairBsdf)
         case microfaceReflection(MicrofacetReflection)
 
@@ -16,8 +15,6 @@ indirect enum GlobalBsdfType: GlobalBsdf {
                 case .dielectricBsdf(let bsdf):
                         return bsdf.evaluateWorld(wo: woWorld, wi: wiWorld)
                 case .hairBsdf(let bsdf):
-                        return bsdf.evaluateWorld(wo: woWorld, wi: wiWorld)
-                case .geometricPrimitiveBsdf(let bsdf):
                         return bsdf.evaluateWorld(wo: woWorld, wi: wiWorld)
                 default:
                         fatalError("Unhandled GlobalBsdfType case in evaluateWorld")
@@ -33,8 +30,6 @@ indirect enum GlobalBsdfType: GlobalBsdf {
                 case .dielectricBsdf(let bsdf):
                         return bsdf.probabilityDensityWorld(wo: woWorld, wi: wiWorld)
                 case .hairBsdf(let bsdf):
-                        return bsdf.probabilityDensityWorld(wo: woWorld, wi: wiWorld)
-                case .geometricPrimitiveBsdf(let bsdf):
                         return bsdf.probabilityDensityWorld(wo: woWorld, wi: wiWorld)
                 default:
                         fatalError("Unhandled GlobalBsdfType case in probabilityDensityWorld")
@@ -54,9 +49,6 @@ indirect enum GlobalBsdfType: GlobalBsdf {
                         return bsdf.sampleWorld(wo: woWorld, u: u)
                 case .hairBsdf(let bsdf):
                         return bsdf.sampleWorld(wo: woWorld, u: u)
-                case .geometricPrimitiveBsdf(let bsdf):
-                        // Dispatch the call to the wrapped GlobalBsdfType instance
-                        return bsdf.sampleWorld(wo: woWorld, u: u)
                 default:
                         fatalError("Unhandled GlobalBsdfType case in sampleWorld")
                 }
@@ -72,8 +64,6 @@ indirect enum GlobalBsdfType: GlobalBsdf {
                         return bsdf.evaluateLocal(wo: woLocal, wi: wiLocal)
                 case .hairBsdf(let bsdf):
                         return bsdf.evaluateLocal(wo: woLocal, wi: wiLocal)
-                case .geometricPrimitiveBsdf(let bsdf):
-                        return bsdf.evaluateLocal(wo: woLocal, wi: wiLocal)
                 default:
                         fatalError("Unhandled GlobalBsdfType case in evaluateLocal")
                 }
@@ -87,8 +77,6 @@ indirect enum GlobalBsdfType: GlobalBsdf {
                         return bsdf.albedo()
                 case .dielectricBsdf(let bsdf):
                         return bsdf.albedo()
-                case .geometricPrimitiveBsdf(let bsdf):
-                        return bsdf.albedo()
                 case .hairBsdf(let bsdf):
                         return bsdf.albedo()
                 default:
@@ -100,10 +88,6 @@ indirect enum GlobalBsdfType: GlobalBsdf {
                         case .coatedDiffuseBsdf(let bsdf):
                                 return bsdf.evaluateLocal(wo: woLocal, wi: wiLocal)
                         case .dielectricBsdf(let bsdf):
-                                return bsdf.evaluateLocal(wo: woLocal, wi: wiLocal)
-                        // Add all other concrete BSDF cases...
-                        case .geometricPrimitiveBsdf(let bsdf):
-                                // Recursion for the special case
                                 return bsdf.evaluateLocal(wo: woLocal, wi: wiLocal)
                         case .hairBsdf(let bsdf):
                                 return bsdf.evaluateLocal(wo: woLocal, wi: wiLocal)
@@ -118,10 +102,6 @@ indirect enum GlobalBsdfType: GlobalBsdf {
                 case .coatedDiffuseBsdf(let bsdf):
                         return bsdf.bsdfFrame
                 case .dielectricBsdf(let bsdf):
-                        return bsdf.bsdfFrame
-                // ... add all other concrete BSDF cases ...
-                case .geometricPrimitiveBsdf(let bsdf):
-                        // Recursion for the special recursive case
                         return bsdf.bsdfFrame
                 case .hairBsdf(let bsdf):
                         return bsdf.bsdfFrame

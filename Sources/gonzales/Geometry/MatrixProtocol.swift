cf1ef5b1e4f7fc5a1306dca74f76bb43a1480582
Gon Solo <gonsolo@gmail.com>
2025-11-12 17:50:44 +0100

    Remove.

--- Sources/gonzales/Geometry/MatrixProtocol.swift
+++
@@ -1,8 +0,0 @@
-/// A type that provides a mathematical matrix.
-
-// public protocol MatrixProtocol {
-//        init()
-//        init(m: Self)
-//        var inverse: Self { get }
-//        var isAffine: Bool { get }
-// }

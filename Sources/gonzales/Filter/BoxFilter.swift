50152ee1ea6399abcaae2c97651dcfbb2fa5e75a
Gon Solo <gonsolo@gmail.com>
2025-11-12 18:02:05 +0100

    Box filter sampling.

--- Sources/gonzales/Filter/BoxFilter.swift
+++ Sources/gonzales/Filter/BoxFilter.swift
@@ -2,8 +2,15 @@ struct BoxFilter: Filter {
 
         func evaluate(atLocation _: Point2f) -> FloatX { return 1 }
 
-        func sample(u _: (FloatX, FloatX)) -> FilterSample {
-                unimplemented()
+        func sample(u: (FloatX, FloatX)) -> FilterSample {
+                let f = FilterSample(
+                        location: Point2f(
+                                x: (2 * u.0 - 1) * support.x,
+                                y: (2 * u.1 - 1) * support.y
+                        ),
+                        probabilityDensity: 1
+                )
+                return f
         }
 
         var support: Vector2F

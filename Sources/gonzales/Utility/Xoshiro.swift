f7a054444e2132e63f0170b140dbc70d230ad67f
Gon Solo <gonsolo@gmail.com>
2025-11-12 07:49:38 +0100

    Lint.

--- Sources/gonzales/Utility/Xoshiro.swift
+++ Sources/gonzales/Utility/Xoshiro.swift
@@ -2,23 +2,21 @@
 
 struct Xoshiro: RandomNumberGenerator {
 
-        typealias StateType = (UInt64, UInt64, UInt64, UInt64)
-
-        private var state: StateType = (666, 1_234_567, 565_000_565, 939_393_939_393)
+        private var state: [4 of UInt64] = [666, 1_234_567, 565_000_565, 939_393_939_393]
 
         public mutating func next() -> UInt64 {
                 // Derived from public domain implementation of xoshiro256** here:
                 // http://xoshiro.di.unimi.it
                 // by David Blackman and Sebastiano Vigna
-                let x = state.1 &* 5
+                let x = state[1] &* 5
                 let result = ((x &<< 7) | (x &>> 57)) &* 9
-                let t = state.1 &<< 17
-                state.2 ^= state.0
-                state.3 ^= state.1
-                state.1 ^= state.2
-                state.0 ^= state.3
-                state.2 ^= t
-                state.3 = (state.3 &<< 45) | (state.3 &>> 19)
+                let t = state[1] &<< 17
+                state[2] ^= state[0]
+                state[3] ^= state[1]
+                state[1] ^= state[2]
+                state[0] ^= state[3]
+                state[2] ^= t
+                state[3] = (state[3] &<< 45) | (state[3] &>> 19)
                 return result
         }
 }

50299877aaadf40fad1d4d21f6f59f0f527ce8f2
Andreas Wendleder <andreas.wendleder@gmail.com>
2026-03-25 01:34:25 +0100

    Fix Swift 6 SPM plugin deprecation warnings

--- Plugins/SobolGeneratorPlugin/plugin.swift
+++ Plugins/SobolGeneratorPlugin/plugin.swift
@@ -7,23 +7,23 @@ struct SobolGeneratorPlugin: BuildToolPlugin {
         let generatorExe = try context.tool(named: "SobolGenerator")
         
         // Input path (the direction numbers file)
-        let directionNumbersPath = context.package.directory
-            .appending("Sources")
-            .appending("libgonzales")
-            .appending("Resources")
-            .appending("new-joe-kuo-6.21201")
+        let directionNumbersPath = context.package.directoryURL
+            .appendingPathComponent("Sources")
+            .appendingPathComponent("libgonzales")
+            .appendingPathComponent("Resources")
+            .appendingPathComponent("new-joe-kuo-6.21201")
         
         // Output path (generated source file)
-        let outputPath = context.pluginWorkDirectory.appending("SobolMatrices.swift")
+        let outputPath = context.pluginWorkDirectoryURL.appendingPathComponent("SobolMatrices.swift")
         
         // Create build command
         return [
             .buildCommand(
                 displayName: "Generating Sobol matrices from direction numbers",
-                executable: generatorExe.path,
+                executable: generatorExe.url,
                 arguments: [
-                    directionNumbersPath.string,
-                    outputPath.string
+                    directionNumbersPath.path,
+                    outputPath.path
                 ],
                 inputFiles: [directionNumbersPath],
                 outputFiles: [outputPath]
